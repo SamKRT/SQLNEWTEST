6171d806a7cb25f7152a99a50a358bdec4cf1fee
Samuel Kraut <kraut677@gmail.com>
2021-05-19 16:13:04 +0200

    Add project files.

---
+++ SQLNEWTEST/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SQLNEWTEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queryManager = new SQLQueryManager();
+            queryManager.InsertMitarbeiter("Samuel", "Kraut", new DateTime(2000, 6, 16));
+        }
+    }
+}
---
+++ SQLNEWTEST/SQLQueryManager.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using System;
+using System.Data.SqlClient;
+
+namespace SQLNEWTEST
+{
+    class SQLQueryManager
+    {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Samuel Kraut\Documents\Test1.mdf;Integrated Security=True;Connect Timeout=30";
+                        
+        private readonly static string InsertMitarbeiterQuery = "INSERT INTO Mitarbeiter(Vorname, Nachname, Geburtstag) VALUES ('@Vorname','@Nachname','@Geburtstag')";
+        
+        public void InsertMitarbeiter(string vorname, string nachname, DateTime geburtstag)
+        {
+            using var connection = new SqlConnection(connectionString);
+            connection.Execute(InsertMitarbeiterQuery, new { Vorname = vorname, Nachname = nachname, Geburtstag = geburtstag });
+        }
+    }
+}
